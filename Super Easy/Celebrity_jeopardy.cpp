791f2ea888d83def7d149d20a042f34b5ebd7f55
baiching <bachmarma@gmail.com>
2020-08-23 13:16:22 +0600

    Solved Celebrity jeopardy problem

---
+++ Super Easy/Celebrity_jeopardy.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    char equation[1000000];
+
+    while(gets(equation)){
+        printf("%s\n", equation);
+    }
+
+    return 0;
+}
